f3a718e4fb3d3d8a8d39a9f84e5c76e306be315b
Nitin Kushwaha <ansingh6388@gmail.com>
2025-07-09 22:51:41 +0530

    Update settings.py

--- cloudtowebsite/settings.py
+++ cloudtowebsite/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "cloudtowebsite.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         default = os.getenv("DATABASE_URL"),
-        conn_max_age = 600',
+        conn_max_age = 600,
     )   
 }
 
