5cbe7ddb50004d2033f59d9cbdfebd154f303b71
Nitin Kushwaha <ansingh6388@gmail.com>
2025-07-09 20:58:30 +0530

    Update views.py

--- media/views.py
+++ media/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from .models import Photo, Video
+
+def home(request):
+    photos = Photo.objects.all()
+    videos = Video.objects.all()
+    return render(request, 'home.html', {'photos': photos, 'videos': videos})
+
+
 
