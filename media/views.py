352043637f5e1507cb0edd05d7019c4792ce8ff7
Nitin Kushwaha <ansingh6388@gmail.com>
2025-07-10 20:35:51 +0530

    Update views.py

--- media/views.py
+++ media/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Photo, Video
+from django.core.management import call_command
+from django.http import HttpResponse
 
 def home(request):
     photos = Photo.objects.all()
     videos = Video.objects.all()
     return render(request, 'home.html', {'photos': photos, 'videos': videos})
+
+
+
+def create_admin_on_render(request):
+    try:
+        call_command('loaddata', 'superuser.json')
+        return HttpResponse("✅ Superuser created successfully!")
+    except Exception as e:
+        return HttpResponse(f"❌ Error: {e}")
 
 
 
