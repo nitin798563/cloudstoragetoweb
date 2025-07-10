5633944f1dba283171c21d052e338cf8b5eb1a9e
Nitin Kushwaha <ansingh6388@gmail.com>
2025-07-10 21:52:52 +0530

    Update views.py

--- media/views.py
+++ media/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from .models import Photo, Video
-from django.core.management import call_command
-from django.http import HttpResponse
 
 def home(request):
     photos = Photo.objects.all()
@@ -10,20 +8,6 @@ def home(request):
 
 
 
-def create_admin_on_render(request):
-    try:
-        call_command('loaddata', 'superuser.json')
-        return HttpResponse("✅ Superuser created successfully!")
-    except Exception as e:
-        return HttpResponse(f"❌ Error: {e}")
-
-def migrate_view(request):
-    try:
-        call_command('makemigrations')
-        call_command('migrate')
-        return HttpResponse("✅ Migration done successfully!")
-    except Exception as e:
-        return HttpResponse(f"❌ Migration error: {e}")
 
 
 
