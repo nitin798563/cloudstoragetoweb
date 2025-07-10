dc6cf58ffbde050dd624332e36780d1956c0031f
Nitin Kushwaha <ansingh6388@gmail.com>
2025-07-10 19:01:57 +0530

    Update 0001_initial.py

--- media/migrations/0001_initial.py
+++ media/migrations/0001_initial.py
@@ -1,5 +1,4 @@
-
-
+import cloudinary.models
 from django.db import migrations, models
 
 
@@ -11,19 +10,45 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Photo',
+            name="Photo",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('image_url', models.URLField(help_text='Upload photo to Cloudinary and paste URL here')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                (
+                    "image",
+                    cloudinary.models.CloudinaryField(
+                        max_length=255, verbose_name="image"
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Video',
+            name="Video",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('video_url', models.URLField(help_text='Upload video to ImageKit.io and paste URL here')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                (
+                    "video_url",
+                    models.URLField(
+                        help_text="Upload video to ImageKit.io and paste URL here"
+                    ),
+                ),
             ],
         ),
     ]
